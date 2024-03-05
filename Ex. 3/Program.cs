d3ab711af5b774c82f22e4210d4dc54b7da0b3dc
chloe <chloe@DESKTOP-MHQ5M5I>
2024-03-05 19:41:10 -0300

    Adicionar arquivos de projeto.

---
+++ Ex. 3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Digite a quantidade de valores que quer ler: ");
+
+int n = int.Parse(Console.ReadLine());
+double[] val = new double[n];
+double soma = 0;
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Digite o {i + 1}º valor: ");
+    val[i] = double.Parse(Console.ReadLine());
+    soma += val[i];
+}
+
+double media = soma / n;
+Console.WriteLine($"A média aritmética dos valores é: {media}");
